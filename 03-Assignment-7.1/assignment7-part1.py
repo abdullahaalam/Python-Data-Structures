050a7f312102becf25cd0ba1b866f126f076033f
Abdullah Alam <abdullah.alam.omi@gmail.com>
2015-12-17 20:35:58 -0500

    added Python for Everybody Specialization 2 of 5 Courses

---
+++ 03-Assignment-7.1/assignment7-part1.py
@@ -0,0 +1,14 @@
+# Write a program that prompts for a file name, then opens that file and reads 
+# through the file, and print the contents of the file in upper case. Use the file words.txt to 
+# produce the output below.
+# You can download the sample data at http://www.pythonlearn.com/code/words.txt
+
+
+
+# Use words.txt as the file name
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+for line in fname:
+	line = line.rstrip()
+inp = fh.read()
+print inp.upper()
